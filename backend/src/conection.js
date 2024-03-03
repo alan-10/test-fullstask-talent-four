aae395870cf431f9c9bd9fd8c33c98f1078c5708
Alan Diniz <alanzinid@gmail.com>
2024-03-03 12:03:10 -0300

    remove comments

--- backend/src/conection.js
+++ backend/src/conection.js
@@ -6,26 +6,13 @@ const driver = neo4j.driver('bolt://3.216.27.176:7687',
   neo4j.auth.basic('neo4j', 'master-vibrations-retrievals'),
   {/* encrypted: 'ENCRYPTION_OFF' */ });
 
-driver.session({ database: "neo4j" });
+const s  = driver.session({ database: "neo4j" });
 
+//clear data base
 // s.run(`
 //   MATCH (u:User )
 //   DETACH DELETE u
 // `)
-
-// session.run(query, params)
-//   .then((result) => {
-//     console.log('result', result);
-//     result.records.forEach((record) => {
-//         console.log(record.get('ip'));
-//     });
-//     session.close();
-//     driver.close();
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//   });
-
 
 
 module.exports = {
